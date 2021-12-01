f4bddf44d4186c0ffb1d131219d91b086fd0445b
Kostra0ne <guillaume.amangoua@gmail.com>
2021-12-01 13:02:43 +0100

    added m1w1d3 snippets > positions review + js intro

---
+++ module-1/week-1/day-3/js-intro/5-if-else-statements.js
@@ -0,0 +1,29 @@
+// if statement
+
+let score = 10;
+let conditionToCheck = score >= 10;
+
+if (conditionToCheck) {
+  console.log("it's a win");
+} else {
+  console.log("sorry, you lost : try again");
+}
+
+let amIHungry = true;
+
+if (amIHungry === true) {
+  console.log("time to eat");
+}
+
+console.log("bla bla");
+
+const finalResult = -10;
+console.log("---------------------");
+
+if (finalResult < 10) {
+    console.log("you suck at this game friend");
+} else if (finalResult > 10 && finalResult < 100) {
+    console.log("you did ok friend");
+}  else {
+    console.log("you rock at this game ! congratz");
+}
