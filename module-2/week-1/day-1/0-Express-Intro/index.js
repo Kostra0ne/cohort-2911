1e555401d848c01e42d366c416de4831fb52fc75
Flo <florian.aube@gmail.com>
2022-01-03 17:14:05 +0100

    Added some comments and explaination on the basic Express setup

--- module-2/week-1/day-1/0-Express-Intro/index.js
+++ module-2/week-1/day-1/0-Express-Intro/index.js
@@ -1,8 +1,25 @@
+// https://expressjs.com/
 const express = require("express");
-const app = express();
 
+// Import the express API into our file in order to use
+// some of it's functionalities
+const app = express();
+// express is a function that returns an object
+// that has methods that allows us to create a web server
+
+// app.get("/",function(request,response,next){
+// response.send("<h1>Hello ! </h1>")
+// })
+
+/**
+ * In NodeJS, every module (file) has access to a variable
+ * named __dirname,  representing the absolute path of the file currently executed
+ */
 console.log("Absolute path is: ", __dirname);
 
+// Tell express where the static assets will be served from (css,images,videos,js(DOM...), not HTML 😬) in this case, the public folder
+// This allows the browser to get these assets without having to ask for permissions
+// and without having to create seperate routes for these type of files
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (request, response) => {
@@ -44,6 +61,7 @@ app.get("/contacts", (req, res) => {
 });
 
 // Fetching CSS files without express.static
+
 // app.get("/css/style.css", (req, res) => {
 // 	res.sendFile("/css/style.css");
 // });
--- module-2/week-1/day-1/1-Express-hbs/index.js
+++ module-2/week-1/day-1/1-Express-hbs/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+
 const app = express();
+
 const PORT = 4000;
 const hbs = require("hbs");
 
+// You will need to "Register" your partials, you tell hbs where to look for them 🕵️‍♂️
 hbs.registerPartials(__dirname + "/views/partials");
 
+// Setting the view engine for express to know he is serving hbs files
 app.set("view engine", "hbs");
+
+/** https://expressjs.com/en/starter/static-files.html  */
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -36,9 +42,9 @@ app.get("/users", (req, res) => {
 			picture: "https://randomuser.me/api/portraits/men/5.jpg",
 		},
 	];
-	// x = x + 1
-	// x += 1
-	// users: users
+	// When a key and a variable have the same name, you can on certain instance
+	// Omit the value and just put one keyword, the key (Who has the same name as the variable)
+	// {users: users}   =>   {users}
 	res.render("users", {
 		users,
 		css: ["users.css"],
